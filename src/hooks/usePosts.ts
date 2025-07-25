71948cad1472e611810f83603b370b03db7b3c4b
Anthony Kramer <anthony.kramer@gmail.com>
2025-07-25 13:32:48 -0500

    12- Paginated Queries

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,37 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) => {
+  const fetchPosts = () =>
+    axios
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
+        params: {
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize
+        }
+      })
+      .then((res) => res.data);
+
+    return useQuery<Post[], Error>({
+    queryKey: ['posts', query],
+    queryFn: fetchPosts,
+    staleTime: 1 * 60 * 1000, //1min
+    placeholderData: keepPreviousData
+    
+  });
+
+}
+
+export default usePosts
