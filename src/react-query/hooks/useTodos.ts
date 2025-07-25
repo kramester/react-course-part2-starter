bef9e8f4490969f9bf727f001422aff7984b13d7
Anthony Kramer <anthony.kramer@gmail.com>
2025-07-25 15:04:31 -0500

    20- Creating a Reusable API Client

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
+import APIClient from "../services/apiClient";
+
+const apiClient = new APIClient<Todo>('/todos')
 
 export interface Todo {
   id: number;
@@ -9,16 +11,10 @@ export interface Todo {
   completed: boolean;
 }
 
-
 const useTodos = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => res.data);
-
     return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: fetchTodos,
+    queryFn: apiClient.getAll,
     staleTime: 10 * 1000 //10sec
   });
 
