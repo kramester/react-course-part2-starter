43f23e10557a39b9f6bb9170a89af33b52e8a139
Anthony Kramer <anthony.kramer@gmail.com>
2025-07-25 14:19:55 -0500

    17- Showing Mutation Progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -21,6 +21,8 @@ const TodoForm = () => {
         savedTodo,
         ...(todos || []),
       ]);
+
+      if (ref.current) ref.current.value = '';
     },
   });
   const ref = useRef<HTMLInputElement>(null);
@@ -47,7 +49,9 @@ const TodoForm = () => {
           <input ref={ref} type='text' className='form-control' />
         </div>
         <div className='col'>
-          <button className='btn btn-primary'>Add</button>
+          <button disabled={addTodo.isPending} className='btn btn-primary'>
+            {addTodo.isPending ? 'Adding...' : 'Add'}
+          </button>
         </div>
       </form>
     </>
