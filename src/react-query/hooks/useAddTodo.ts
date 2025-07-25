aea21856a5e8547b1052862063618642aa9f3c35
Anthony Kramer <anthony.kramer@gmail.com>
2025-07-25 15:09:08 -0500

    21- Creating a Reusable HTTP Service

--- src/react-query/hooks/useAddTodo.ts
+++ src/react-query/hooks/useAddTodo.ts
@@ -1,21 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Todo } from "./useTodos";
-import axios from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIClient from "../services/apiClient";
 
-const apiClient = new APIClient<Todo>('/todos')
+import todoService, { Todo }  from "../services/todoService";
 
 interface AddTodoContext {
   previousTodos: Todo[];
 }
 
-
 const useAddTodo = (onAdd: () => void) => {
   const queryClient = useQueryClient();
   
   return useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: apiClient.post,
+    mutationFn: todoService.post,
 
     onMutate: (newTodo: Todo) => {
       const previousTodos = queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
